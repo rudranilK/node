621c6949d621548e0a64efa442cbe312118d8549
rudranilK <rudranilkundu@gmail.com>
2023-01-09 20:38:31 +0530

    added vscode folder in gitignore

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,11 @@
 const User = require("../schema/user.schema");
+const Post = require("../schema/post.schema");
 
 module.exports.getUsersWithPostCount = async (req, res) => {
   try {
     //TODO: Implement this API
+
+
 
     res.status(200).json({});
   } catch (error) {
